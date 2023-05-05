9ce0808682e22671187c7fe3b745a356278ea20d
JordanNitta <Jordannittarodrigues@gmail.com>
2023-05-05 12:59:49 -0700

    first commit

---
+++ src/main/java/com/jordan/daichiroutes/DaichiRoutesApplication.java
@@ -0,0 +1,16 @@
+package com.jordan.daichiroutes;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DaichiRoutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaichiRoutesApplication.class, args);
+	}
+	
+	
+}
---
+++ src/test/java/com/jordan/daichiroutes/DaichiRoutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jordan.daichiroutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaichiRoutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
